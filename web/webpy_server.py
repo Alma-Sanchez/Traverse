9726332b63696f0442b0183f28d5c0ffbcddd123
EricaJurado <ej3535a@student.american.edu>
2015-05-27 23:29:28 -0400

    Update webpy_server.py

--- web/webpy_server.py
+++ web/webpy_server.py
@@ -1,25 +1,34 @@
 import web
 import sqlite3
 
-#to get user's ip: web.ctx.ip
-
 urls = ('/','main')
 app = web.application(urls, globals(), True)
 render=web.template.render('templates/')
 
-connection=sqlite3.connect('test.db')
-c=connection.cursor()
-sql="SELECT * FROM nameoftable WHERE ascension=?"
-
 def readdata(x):
+	connection=sqlite3.connect('test.db',check_same_thread=False)
+	c=connection.cursor()
+	sql="SELECT * FROM nameoftable WHERE ascension=?"
 	for row in c.execute(sql, [(x)]):
 		return True
 	else:
 		return False
+		
+def checkip(x):
+	ipcon=sqlite3.connect('ip.db',  check_same_thread=False)
+	ipc=ipcon.cursor()
+	ipsql="SELECT * FROM iptable WHERE ip=?"
+	for row in ipc.execute(ipsql, [(x)]):
+		return True
+	else:
+		ipc.execute("INSERT INTO iptable VALUES('127.0.0.1')")
+		ipcon.commit()
+		return True
 
 class main:
 	def GET(self):
-		return render.main()
+		if checkip(web.ctx.environ['REMOTE_ADDR'])==True:
+			return render.main()
 
 	def POST(self):
 		form =  web.input()
