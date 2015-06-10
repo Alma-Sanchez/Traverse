43a18065259b36ff369dc7410adbba3817dd7ba6
ASandhuZero <as7687a@american.edu>
2015-06-10 18:54:41 -0400

    Added Docstring style comments.

--- db_creator.py
+++ db_creator.py
@@ -7,6 +7,15 @@ import sqlite3
 conn = sqlite3.connect("SAAM_database_test2.db")
 
 def dropTables():
+	"""
+	This function drops all the tables that should be in the SAAM database.
+
+	Parameters:
+	None
+
+	Returns:
+	None
+	"""
 	c = conn.cursor()
 
 	c.execute("DROP TABLE IF EXISTS Player_Data")
@@ -19,18 +28,38 @@ def dropTables():
 	c.execute("DROP TABLE IF EXISTS Accession")
 
 def createTables():
+	"""This function creates the tables that will be used for the SAAM database. 
+	If this function is executed while these tables exist, an error will appear. 
+	In order to change the schema, call dropTables() and call createTables().
+
+	Parameters:
+	None
+
+	Returns:
+	None
+	"""
 	c = conn.cursor()
 
-	c.execute("CREATE TABLE IF NOT EXISTS Player_Data (Player_ID INT, Player_name TEXT, e_mail TEXT, Password TEXT)")
-	c.execute("CREATE TABLE IF NOT EXISTS Player_Action (Action_ID INT, Player_ID INT, Current_step_ID INT, Previous_step_ID INT, Story_ID INT)")
-	c.execute("CREATE TABLE IF NOT EXISTS Archived_Player_Data (Player_ID INT, Player_name TEXT, e_mail, TEXT, Password TEXT)")
-	c.execute("CREATE TABLE IF NOT EXISTS Archived_Player_Action (Action_ID INT, Player_ID INT, Current_step_ID INT, Previous_step_ID INT, Story_ID INT)")
-	c.execute("CREATE TABLE IF NOT EXISTS Story_Data (Story_ID INT, Character_ID INT, Title_of_Story TEXT, Number_of_steps INT)")
-	c.execute("CREATE TABLE IF NOT EXISTS Character_Data(Character_ID INT, Character_name TEXT, Accession_number TEXT)")
-	c.execute("CREATE TABLE IF NOT EXISTS Step_Data(Step_ID INT, Story_ID INT, Previous_step_ID INT, Next_Step_ID INT, Accession_association TEXT, Hint TEXT, Art TEXT, Step_Text TEXT)")
-	c.execute("CREATE TABLE IF NOT EXISTS Accession(Accession_association TEXT, Accession_number TEXT)")
+	c.execute("CREATE TABLE Player_Data (Player_ID INT, Player_name TEXT, e_mail TEXT, Password TEXT)")
+	c.execute("CREATE TABLE Player_Action (Action_ID INT, Player_ID INT, Current_step_ID INT, Previous_step_ID INT, Story_ID INT)")
+	c.execute("CREATE TABLE Archived_Player_Data (Player_ID INT, Player_name TEXT, e_mail, TEXT, Password TEXT)")
+	c.execute("CREATE TABLE Archived_Player_Action (Action_ID INT, Player_ID INT, Current_step_ID INT, Previous_step_ID INT, Story_ID INT)")
+	c.execute("CREATE TABLE Story_Data (Story_ID INT, Character_ID INT, Title_of_Story TEXT, Number_of_steps INT)")
+	c.execute("CREATE TABLE Character_Data(Character_ID INT, Character_name TEXT, Accession_number TEXT)")
+	c.execute("CREATE TABLE Step_Data(Step_ID INT, Story_ID INT, Previous_step_ID INT, Next_Step_ID INT, Accession_association TEXT, Hint TEXT, Art TEXT, Step_Text TEXT)")
+	c.execute("CREATE TABLE Accession(Accession_association TEXT, Accession_number TEXT)")
 
 def populateTables():
+	"""
+	This function will populate tables within the SAAM database using CSV files.
+	The file names and the path for the file open should be the same. If it is not then the reader will break.
+
+	Parameters:
+	None 
+
+	Returns:
+	None
+	"""
 	c = conn.cursor()
 
 	with open('CSV_file\Accession.csv','rb') as accession_data_file:
