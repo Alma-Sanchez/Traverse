dcb32642a54bc6d9ffd57eb0f1e53732d022f5f4
Alma Sanchez <as0820a@american.edu>
2015-06-10 15:15:23 -0400

    Templates html and css

---
+++ Templates/webpy_server.py
@@ -0,0 +1,36 @@
+import web
+# import sqlite3
+
+urls = (
+    '/','index'
+)
+
+app = web.application(urls, globals())
+render = web.template.render('templates/')
+
+# connection=sqlite3.connect('test.db')
+# c=connection.cursor()
+# sql="SELECT * FROM nameoftable WHERE ascension=?"
+
+# def readdata(x):
+#   for row in c.execute(sql, [(x)]):
+#       return True
+#   else:
+#       return False
+
+class index:
+    def GET(self):
+        #renders the html file
+        # return render.mainScreen()
+        return render.charScreen()
+        # return render.storyScreen()
+
+
+
+#   def POST(self):
+        # form =  web.input()
+        # form = form.user
+
+ 
+if __name__ == "__main__": 
+    app.run()
