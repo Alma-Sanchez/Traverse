c1201d6c23d88c2307685be926b6f214f8dd30d6
Totemtastikk <ev4835a@student.american.edu>
2015-06-12 12:41:48 -0400

    Reverted Change

--- BackEndSkeleton/Server.py
+++ BackEndSkeleton/Server.py
@@ -2,10 +2,7 @@
 import HTTPHandler #Imports modules needed to run server.py
 import DBManager
 
-
-def setupServer():
-	server = HTTPHandler.WebpyServer() #Creates a new instance of the server.
-	server.app.run() #Runs the server if server.py is the file being called. WILL NOT WORK IF SERVER.PY IS BEING CALLED AS A MODULE.
+server = HTTPHandler.WebpyServer() #Creates a new instance of the server.
 
 if __name__=="__main__": #Checks to see if server.py is the main file that is being called. 
-	setupServer()
+	server.app.run() #Runs the server if server.py is the file being called. WILL NOT WORK IF SERVER.PY IS BEING CALLED AS A MODULE.
