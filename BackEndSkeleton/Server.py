df0178d120110a67384f68fa9a2c12d87ef641c1
Totemtastikk <ev4835a@student.american.edu>
2015-06-12 12:42:52 -0400

    Created the serverSetup() function to be called by the main function.

--- BackEndSkeleton/Server.py
+++ BackEndSkeleton/Server.py
@@ -2,7 +2,9 @@
 import HTTPHandler #Imports modules needed to run server.py
 import DBManager
 
-server = HTTPHandler.WebpyServer() #Creates a new instance of the server.
+def setupServer():
+	server = HTTPHandler.WebpyServer() #Creates a new instance of the server.
+	server.app.run() #Runs the server if server.py is the file being called. WILL NOT WORK IF SERVER.PY IS BEING CALLED AS A MODULE.
 
 if __name__=="__main__": #Checks to see if server.py is the main file that is being called. 
-	server.app.run() #Runs the server if server.py is the file being called. WILL NOT WORK IF SERVER.PY IS BEING CALLED AS A MODULE.
+	setupServer()
