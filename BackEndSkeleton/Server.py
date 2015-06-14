4fc2f03831a28046eeb58f2a4a4d2b19136fbd56
Totemtastikk <ev4835a@student.american.edu>
2015-06-14 19:19:54 -0400

    Added comments to the setupServer function

--- BackEndSkeleton/Server.py
+++ BackEndSkeleton/Server.py
@@ -3,8 +3,17 @@ import HTTPHandler #Imports modules needed to run server.py
 import DBManager
 
 def setupServer():
+	"""
+	This function creates a Webpy server obect and begins to run that server.
+
+	Parameters:
+	None
+
+	Returns:
+	None
+	"""
 	server = HTTPHandler.WebpyServer() #Creates a new instance of the server.
 	server.app.run() #Runs the server if server.py is the file being called. WILL NOT WORK IF SERVER.PY IS BEING CALLED AS A MODULE.
 
 if __name__=="__main__": #Checks to see if server.py is the main file that is being called. 
-	setupServer()
+	setupServer() #runs a Webpy server
