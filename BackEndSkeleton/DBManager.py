8f7c379f1bafb47b6ce2bfdbb40d05b308c6ffaa
ASandhuZero <as7687a@american.edu>
2015-06-14 13:06:26 -0400

    Changed DBManager's function syntax to make use of formal parameters.

--- BackEndSkeleton/DBManager.py
+++ BackEndSkeleton/DBManager.py
@@ -13,6 +13,9 @@ def getPlayerFromDB(ip):
 
     Returns:
         tuple: A row from the Player_Data table associated with the player's IP address.
+
+    Examples:
+        getPlayerFromDB(playerinfo.ip) => (1,1.1.1.1,2)
     """
     cur = conn.cursor()
     cur.execute("select * from Player_Data where IP=:ip", {"ip": ip})
@@ -33,7 +36,7 @@ def getCurrentPlayerActionFromDB(currentStepID):
         getCurrentPlayerActionFromDB(playerInfo.currentStepID) => (4 50, 22, 21, 2, 1)
     """
     cur = conn.cursor()
-    cur.execute("select * from Player_Action where Current_Step_ID =:current_Step_ID", {"Current_Step_ID": Current_Step_ID})
+    cur.execute("select * from Player_Action where Current_Step_ID =:currentStepID", {"currentStepID": currentStepID})
     return cur.fetchone()
 
 
@@ -52,85 +55,85 @@ def getPrevioustPlayerActionFromDB(previousStepID):
         getPlayerActionFromDB(playerInfo.PreviousStepID) => (4 50, 22, 21, 2, 1)
     """
     cur = conn.cursor()
-    cur.execute("select * from Player_Action where Previous_Step_ID=:Previous_Step_ID", {"Previous_Step_ID": Previous_Step_ID})
+    cur.execute("select * from Player_Action where Previous_Step_ID=:previousStepID", {"previousStepID": previousStepID})
     return cur.fetchone()
 
 
-def getCharacterFromDB(Player_ID):
+def getCharacterFromDB(playerID):
     """
     This function will return the character the player has most recently selected
     
     Parameters:
-    (Player_ID
+        Player_ID
 
     Returns:
-    tuple: character fields
+        tuple: character fields
     """
     cur = conn.cursor()
-    cur.execute("select Character_ID from Player_Action where Character_ID=:Character_ID", {"Character_ID": Character_ID})
+    cur.execute("select Character_ID from Player_Action where Player_ID=:playerID", {"playerID": playerID})
     return cur.fetchone()
     pass
 
-def getStoryFromDB(Player_ID):
+def getStoryFromDB(playerID):
     """
     This function will query the database and return the correct story associated with the player.
 
     Parameters:
-    player_ID
+        Player_ID
 
     Returns:
-    tuple: Story fields
+        tuple: Story fields
     """
     cur = conn.cursor()
-    cur.execute("select Story_ID from Player_Action where max Action_ID in Story_ID=:Story_ID", {"Story_ID": Story_ID})
+    cur.execute("select Story_ID from Player_Action where max Action_ID in Player_ID=:playerID", {"Player_ID": playerID})
     return cur.fetchone()
     pass
 
-def getStepDataFromDB(Current_Step_ID):
+def getStepDataFromDB(currentStepID):
     """
     This function will query the database and return the correct step data for the step the player is currently on.
 
     Parameters:
-    Current_Step_ID
+        Current_Step_ID
 
     Returns:
-    tuple: Step fields
+        tuple: Step fields
     """
     cur = conn.cursor()
-    cur.execute("select * from Step_Data where Current_Step_ID =:Step_ID", {"Step_ID": Current_Step_ID})
+    cur.execute("select * from Step_Data where Current_Step_ID =:Step_ID", {"Step_ID": currentStepID})
     return cur.fetchone()
 
 
-def getAccessionNumberFromDB(Accession_Association):
+def getAccessionNumberFromDB(accessionAssociation):
     cur = conn.cursor()
-    cur.execute("select Accession_Number from Accession_Data where Accession_Number =: Accession_Number", {"Accession_Number": Accesion_Number})
+    cur.execute("select Accession_Number from Accession_Data where Accession_Association =: accessionAssociation", {"accessionAssociation": accessionAssociation})
     return cur.fetchall()
 
 
-def getCharacterData(Current_Character_ID):
+def getCharacterData(currentCharacterID):
     """
     This function will query the database and return the correct character data for the character the player is currently using.
 
     Parameters:
-    Current_Character_ID
+        Current_Character_ID
 
     Returns:
-    tuple: character fields 
+        tuple: character fields 
     """
     cur = conn.cursor()
-    cur.execute("select * from Step_Data where Current_Character_ID =:Character_ID", {"Character_ID": Current_Character_ID})
+    cur.execute("select * from Step_Data where Current_Character_ID =:currentCharacterID", {"currentCharacterID": currentCharacterID})
     return cur.fetchone()
 
-def getStoryData(Current_Story_ID):
+def getStoryData(currentStoryID):
     """
     This function will query the database and return the correct story data for the story the player is currently playing.
 
     Parameters:
-    Current_Story_ID
+        Current_Story_ID
 
     Returns:
-    tuple: story fields
+        tuple: story fields
     """
     cur = conn.cursor()
-    cur.execute("select * from Story_Data where Current_Story_ID =:Story_ID", {"Story_ID": Current_Story_ID})
+    cur.execute("select * from Story_Data where Current_Story_ID =:currentStoryID", {"currentStoryID": currentStoryID})
     return cur.fetchone()
