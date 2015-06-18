a38d05eda2dec08f561cf77f8274aec497566948
ASandhuZero <as7687a@american.edu>
2015-06-18 19:28:59 -0400

    Added more tables into db_creator. Seperated player action table into character action, story action, and step action tables.

--- BackEndSkeleton/db_creator.py
+++ BackEndSkeleton/db_creator.py
@@ -17,9 +17,13 @@ def dropTables(connection):
 	c = conn.cursor() #Cursor for querying the database.
 
 	c.execute("DROP TABLE IF EXISTS Player_Data") #Drops (deletes) table named Player_Data if it exists.
-	c.execute("DROP TABLE IF EXISTS Player_Action") #Drops (deletes) table named Player_Action if it exists.
+	c.execute("DROP TABLE IF EXISTS Player_Character_Action") #Drops (deletes) table named Player_Action if it exists.
+	c.execute("DROP TABLE IF EXISTS Player_Story_Action")
+	c.execute("DROP TABLE IF EXISTS Player_Step_Action")
 	c.execute("DROP TABLE IF EXISTS Archived_Player_Data") #Drops (deletes) table named Archived_Player_Data if it exists.
-	c.execute("DROP TABLE IF EXISTS Archived_Player_Action") #Drops (deletes) table named Archived_Player_Action if it exists.
+	c.execute("DROP TABLE IF EXISTS Archived_Player_Character_Action") #Drops (deletes) table named Archived_Player_Action if it exists.
+	c.execute("DROP TABLE IF EXISTS Archived_Player_Story_Action")
+	c.execute("DROP TABLE IF EXISTS Archived_Player_Step_Action")
 	c.execute("DROP TABLE IF EXISTS Story_Data") #Drops (deletes) table named Story_Data if it exists.
 	c.execute("DROP TABLE IF EXISTS Character_Data") #Drops (deletes) table named Character_Data if it exists.
 	c.execute("DROP TABLE IF EXISTS Step_Data") #Drops (deletes) table named Step_Data if it exists.
@@ -41,9 +45,13 @@ def createTables(connection):
 	c = conn.cursor() #Cursor for querying database.
 
 	c.execute("CREATE TABLE Player_Data (Player_ID Integer primary key autoincrement, IP Text, Current_Action_ID INT)") #Creates new table named Player_Data with hardcoded parameters.
-	c.execute("CREATE TABLE Player_Action (Action_ID Integer primary key autoincrement, Player_ID INT, Current_Step_ID INT, Previous_Step_ID INT, Current_Story_ID INT, Current_Character_ID INT, Player_Input Text)") #Creates new table named Player_Action with hardcoded parameters.
+	c.execute("CREATE TABLE Player_Character_Action (Character_Action_ID Integer primary key autoincrement, Player_ID INT, Current_Character_ID INT, Player_Input Text)") #Creates new table named Player_Action with hardcoded parameters.
+	c.execute("CREATE TABLE Player_Story_Action (Story_Action_ID Integer primary key autoincrement, Player_ID INT, Current_Story_ID INT, Player_Input Text)")
+	c.execute("CREATE TABLE Player_Step_Action (Step_Action_ID Integer primary key autoincrement, Previous_Step_ID INT, Current_Step_ID INT, Next_Step_ID INT, Player_Input Text)")
 	c.execute("CREATE TABLE Archived_Player_Data (Player_ID INT, Player_name TEXT, Current_step_ID INT)") #Creates new table named Archived_Player_Data with hardcoded parameters.
-	c.execute("CREATE TABLE Archived_Player_Action (Action_ID INT, Player_ID INT, Current_step_ID INT, Previous_step_ID INT, Story_ID INT)") #Creates new table named Archived_Player_Action with hardcoded parameters.
+	c.execute("CREATE TABLE Archived_Player_Character_Action (Character_Action_ID Int, Player_ID INT, Current_Character_ID INT, Player_Input Text)") #Creates new table named Player_Action with hardcoded parameters.
+	c.execute("CREATE TABLE Archived_Player_Story_Action (Story_Action_ID Int, Player_ID INT, Current_Story_ID INT, Player_Input Text)")
+	c.execute("CREATE TABLE Archived_Player_Step_Action (Step_Action_ID Int, Previous_Step_ID INT, Current_Step_ID INT, Next_Step_ID INT, Player_Input Text)")
 	c.execute("CREATE TABLE Story_Data (Story_ID INT, Character_ID INT, Title_Of_Story TEXT, Number_Of_Steps INT)") #Creates new table named Story_Data with hardcoded parameters.
 	c.execute("CREATE TABLE Character_Data(Character_ID INT, Character_Name TEXT, Accession_Number TEXT)") #Creates new table named Character_Data with hardcoded parameters.
 	c.execute("CREATE TABLE Step_Data(Story_ID INT, Step_ID INT, Previous_step_ID INT, Next_Step_ID INT, Accession_Association TEXT, Step_Text TEXT, Step_Art TEXT, Step_Hint TEXT)") #Creates new table named Step_Data with hardcoded parameters.
