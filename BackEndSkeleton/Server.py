358072b66ebfe0e1918e5c6485beebd6634f3c94
Totemtastikk <ev4835a@student.american.edu>
2015-06-25 13:49:11 -0400

    Delete unused comment.

--- BackEndSkeleton/Server.py
+++ BackEndSkeleton/Server.py
@@ -21,13 +21,3 @@ def setupPlayer():
 
 if __name__=="__main__": #Checks to see if server.py is the main file that is being called. 
 	setupServer() #runs a Webpy server
-
-'''
-while True:
-	#Need to update player on a loop here.
-	#Problem here is that we will constantly be overwriting the player variable with a new PlayerState construction.
-	#Need to be able to update player member variables without overwriting the player variable everytime we update.
-	#We cannot define player outside of the loop because we need to create a new player variable everytime a new player joins the game.
-	#Therefore, creating an instance of the PlayerState object must happen in some sort of loop or some other dynamic construct.
-	setupPlayer()
-'''
