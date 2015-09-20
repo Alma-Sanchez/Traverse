4a1446ae62e3d228e869e14cd62a090bf3d05858
Alma-Sanchez <alma08sanchez@gmail.com>
2015-09-20 16:24:14 -0400

    Removed extra files

--- BackEndSkeleton/static/Styles/screenDimensions.js
+++
@@ -1,7 +0,0 @@
-var widthDim = window.innerWidth
-|| document.documentElement.clientWidth
-|| document.body.clientWidth;
-
-var heightDim = window.innerHeight
-|| document.documentElement.clientHeight
-|| document.body.clientHeight;
