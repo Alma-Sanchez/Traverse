e0632191a708c8cf1003d087a1d6f695a7eb3934
Taylor Heywood <theywood96@gmail.com>
2016-05-06 10:32:35 -0400

    the csv files now also in branching

--- BackEndSkeleton/db_creator.py
+++ BackEndSkeleton/db_creator.py
@@ -80,28 +80,28 @@ def populateTables(connection):
 	"""
 	conn,c = connection
 
-	with open('CSV_file\Accession.csv','rb') as accession_data_file: #Opens file and assigns it to a variable.
+	with open('CSV_file\Text_Accession.csv','rb') as accession_data_file: #Opens file and assigns it to a variable.
 		spamreader = csv.reader(accession_data_file) #Reads the csv file and sets it as a new variable
 		for row in spamreader: #Iterates through csv file rows.
 			c.execute("INSERT INTO Accession_Answers VALUES (?,?,?)", (unicode(row[0], "utf-8"),unicode(row[1], "utf-8"),unicode(row[2], "utf-8"))) #Encodes and inserts data from the csv file.
 	conn.commit() #Commits (permanently changes) the database.
 	accession_data_file.close() #Closes the csv file.
 
-	with open('CSV_file\Character_Data.csv','rb') as character_data_file: #Opens file and assigns it to a variable.
+	with open('CSV_file\Text_Character_Data.csv','rb') as character_data_file: #Opens file and assigns it to a variable.
 		spamreader = csv.reader(character_data_file) #Reads the csv file and sets ut as a new variable.
 		for row in spamreader: #Iterates through csv file rows.
 			c.execute("INSERT INTO Character_Data VALUES (?,?)", (unicode(row[0], "utf-8"),unicode(row[1], "utf-8"))) #Encodes and inserts data from the csv file.
 	conn.commit() #Commits (permanently changes) the database.
 	character_data_file.close() #Closes the csv file.
 
-	with open('CSV_file\Story_Data.csv', 'rb') as story_data_file: #Opens file and assigns it to a variable.
+	with open('CSV_file\Text_Story_Data.csv', 'rb') as story_data_file: #Opens file and assigns it to a variable.
 		spamreader = csv.reader(story_data_file) #Reads the csv file and sets it as a new variable.
 		for row in spamreader: #Iterates through csv file rows.
 			c.execute("INSERT INTO Story_Data VALUES (?,?,?,?,?)", (unicode(row[0], "utf-8"),unicode(row[1], "utf-8"),unicode(row[2], "utf-8"), unicode(row[3], "utf-8"), unicode(row[4], "utf-8"))) #Encodes and inserts data from the csv file.
 	conn.commit() #Commits (permanently changes) the database.
 	story_data_file.close() #Closes the csv file.
 
-	with open('CSV_file\Step_Data.csv', 'rb') as step_data_file: #Opens file and assigns it to a variable.
+	with open('CSV_file\Text_Step_Data.csv', 'rb') as step_data_file: #Opens file and assigns it to a variable.
 		spamreader = csv.reader(step_data_file) #Reads the csv file and sets it a new variable.
 		for row in spamreader: #Iterates through csv file rows.
 			c.execute("INSERT INTO Step_Data VALUES (?,?,?,?,?,?)", (unicode(row[0], "utf-8"),unicode(row[1], "utf-8"),unicode(row[2], "utf-8"),unicode(row[3], "utf-8"),unicode(row[4], "utf-8"),unicode(row[5], "utf-8"))) #Encodes and inserts data from the csv file.
